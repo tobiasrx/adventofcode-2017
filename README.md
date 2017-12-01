4ecdb1136687edddf1981573b38a3c0e86873f48
Tobias Reuleaux <tr@arss-Mac-mini-2.local>
2017-12-01 13:53:43 +0100

    solution of first puzzle day1

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # adventofcode-2017
 
-A Clojure library designed to ... well, that part is up to you.
+Primarily for me to learn closure
 
 ## Usage
 
-FIXME
+
 
 ## License
 
-Copyright © 2017 FIXME
+Copyright © 2017 Tobias Reuleaux
 
 Distributed under the Eclipse Public License either version 1.0 or (at
 your option) any later version.
---
+++ test/adventofcode_2017/day1_test.clj
@@ -0,0 +1,21 @@
+(ns adventofcode-2017.day1-test
+  (:require [clojure.test :refer :all]
+            [adventofcode-2017.day1 :refer :all]))
+
+
+(deftest sample-num-test-1
+  (testing "For Input 1122 it should be 3"
+    (is (= (inverse-captcha 1122) 3))))
+
+(deftest sample-num-test-2
+  (testing "For Input 1111 it should be 4"
+    (is (= (inverse-captcha 1111) 4))))
+
+(deftest sample-num-test-3
+  (testing "For Input 1234 it should be 0"
+    (is (= (inverse-captcha 1234) 0))))
+
+(deftest sample-num-test-4
+  (testing "For Input 91212129 it should be 9"
+    (is (= (inverse-captcha 91212129) 9))))
+
