facde6105211dc43ae605090e9180066b4ea072e
Tobias <tobiasrx@fastmail.com>
2017-12-04 17:07:02 +0100

    refactored, replaced function with reduce, other function with some

--- src/adventofcode_2017/day4_2.clj
+++ src/adventofcode_2017/day4_2.clj
@@ -4,32 +4,10 @@
 
 (defn make-character-map
   [word]
-  (loop [characters word
-         character-map {}]
-    (if (empty? characters)
-      character-map
-      (let [character (first characters)
-            new-character-map (if (contains? character-map character)
-                                (conj character-map {character (inc (get character-map character))})
-                                (conj character-map {character 1})
-                                )]
-        (recur (rest characters) new-character-map)
-        )
-      )
-    )
-  )
-
-(defn contains-map-in-list
-  [map-list map-element]
-  (loop [map-list map-list]
-    (if (empty? map-list)
-      false
-      (if (= map-element (first map-list))
-        true
-        (recur (rest map-list))
-        )
-      )
-    )
+  (reduce (fn [character-map character] (if (contains? character-map character)
+                    (conj character-map {character (inc (get character-map character))})
+                    (conj character-map {character 1})
+                    )) {} word)
   )
 
 (defn is-passphrase-valid
@@ -39,7 +17,7 @@
     (if (empty? words)
       true
       (let [word-character-map (make-character-map (first words))]
-        (if (contains-map-in-list character-map-list word-character-map)
+        (if (some #(= % word-character-map) character-map-list )
           false
           (recur (rest words) (conj character-map-list word-character-map))
           )
